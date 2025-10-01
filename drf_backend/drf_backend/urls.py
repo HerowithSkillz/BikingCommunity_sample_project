b77e26d067b4653b54c4d7cc601d379940c3c3e9
Vignesh Anil Thandessary <heroskillz910@gmail.com>
2025-10-01 17:22:21 +0530

    urls managed and routed, made migrations and created bike table

--- drf_backend/drf_backend/urls.py
+++ drf_backend/drf_backend/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from api.views import BikeViewSet
+from api import auth_views
+
+router = DefaultRouter()
+router.register(r"api", BikeViewSet, basename="API")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/", include(router.urls)),
+
+    #auth end points
+    path("api/auth/register/", auth_views.register_view),
+    path("api/auth/login/", auth_views.login_view),
+    path("api/auth/logout/", auth_views.logout_view),
+    path("api/auth/user/", auth_views.current_user_view),
 ]
