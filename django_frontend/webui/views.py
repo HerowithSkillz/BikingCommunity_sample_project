c8ebb73cc15a3b9be2be3daf5fcb4264f96c0787
Vignesh Anil Thandessary <heroskillz910@gmail.com>
2025-10-01 17:36:22 +0530

    views in webui updated - frontend

--- django_frontend/webui/views.py
+++ django_frontend/webui/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def bikes_page(request):
+    return render(request, "bikes.html")
+
+def login_page(request):
+    return render(request, "login.html")
+
+def register_page(request):
+    return render(request, "register.html")
