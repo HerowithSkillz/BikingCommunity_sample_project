27417b1b0465cc6e2fc896a6619a969e9de2d928
Vignesh Anil Thandessary <heroskillz910@gmail.com>
2025-10-09 16:53:40 +0530

    created bike_handlebar.

---
+++ bike_handlebar/bike_handlebar/views.py
@@ -0,0 +1,7 @@
+import requests
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.conf import settings
+
+AUTH_SERVER = 'http://127.0.0.1:8001' #URL OF bike_fender
+
