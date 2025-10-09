f7709ea532759b599bc8e4c1826c09d7b2ba6476
Vignesh Anil Thandessary <heroskillz910@gmail.com>
2025-10-09 16:40:23 +0530

    User login and register working. Tested on Postman. Tokens received.

--- bike_fender/accounts/serializers.py
+++ bike_fender/accounts/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 
-class RegisterSerializer(serializers.ModelSerialier):
+class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only= True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
@@ -18,9 +18,9 @@ class RegisterSerializer(serializers.ModelSerialier):
     def create(self, validated_data):
         user = User.objects.create(
             username = validated_data['username'],
-            email = validated_data('email'),
-            first_name = validated_data('first_name', ''),
-            last_name = validated_data('last_name', ''),
+            email = validated_data.get('email', ''),
+            first_name = validated_data.get('first_name', ''),
+            last_name = validated_data.get('last_name', ''),
         )
         user.set_password(validated_data['password'])
         user.save()
