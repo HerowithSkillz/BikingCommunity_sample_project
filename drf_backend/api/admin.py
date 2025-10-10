c444b80d2919895e9f6bb7cf25dcb1a52017ad4f
Vignesh Anil Thandessary <heroskillz910@gmail.com>
2025-10-10 21:23:24 +0530

    Deleted previous versions of website.

--- drf_backend/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Bike
-
-@admin.register(Bike)
-class BikeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'brand', 'owner', 'created_at', 'updated_at')
-    list_filter = ('brand', 'owner')
-    search_fields = ('name', 'brand', 'owner__username')
